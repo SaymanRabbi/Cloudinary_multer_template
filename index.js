a4d9f1e14df783910ce702b71d931db197125e22
saymanrabbi <mdrabbi.asm@gmail.com>
2022-10-23 11:16:29 +0600

    mulriple file upload

--- index.js
+++ index.js
@@ -4,26 +4,29 @@ const cloudinary = require('./Clowdinary');
 const app = express();
 const dotenv = require('dotenv').config();
 const fs = require("fs");
-app.post('/file-upload',upload.single('image'),async(req,res)=>{
+app.post('/file-upload',upload.array('image',12),async(req,res)=>{
+      
   try {
-    await cloudinary.v2.uploader.upload(
-      // -------file name what file you want to upload
-        req.file.path,
+    let imgArr = [];
+      for (let i = 0; i < req.files.length; i++) {
+         // -------file name what file you want to upload
+       await cloudinary.v2.uploader.upload(req.files[i].path,
+        // -------folder name where you want to upload
         {
-          // -------folder name where you want to upload
-          folder: 'test',
-        },
-        // -------callback function to get the result
-        (err, response) => {
-          if (err) {
-            removeTmp(req.file.path);
-            return res.status(500).send(err);
-          }
-            res.status(200).send(response);
-            removeTmp(req.file.path);
+          folder:"test"
+       },
+         // -------callback function to get the result
+       (err,result)=>{
+        if (err) {
+          removeTmp(req.file.path);
+          return res.status(500).send(err);
         }
-      );
-    
+        removeTmp(req.files[i].path);
+        imgArr.push({ public_id: result.public_id, url: result.secure_url });
+        res.status(200).send(response);
+       });
+        
+      }
   } catch (error) {
     res.status(500).send(error);
   }
